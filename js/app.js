9dda3e55c9c8e8182535b3df7b86e1a28113367c
peaton373 <peaton373@gmail.com>
2020-03-13 10:30:02 -0500

    added images for evolution of character and makings of CSS/index.html setup

---
+++ js/app.js
@@ -0,0 +1,71 @@
+console.log("Tama-gotcha!")
+
+
+
+/// create class for tamagotchi
+
+class Tamagotchi  {
+	constructor(hunger, sleepiness, boredom, age, name) {
+		this.hunger = hunger
+		this.sleepiness = sleepiness
+		this.boredom = boredom
+		this.age = age
+		this.name = name
+	}
+}
+
+/// hunger (1-10 scale)
+/// sleepiness (1-10 scale)
+/// boredom (1-10 scale)
+/// age
+/// name
+
+
+
+
+
+// create game object
+ const game = {
+	// include methods in game object
+
+
+}
+
+
+
+
+
+// 
+
+// ability to feed pet
+// Increase your pet's hunger, sleepiness, and boredom metrics at intervals of your choosing.
+/// You pet should die if hunger, boredom, or sleepiness hits 10.
+
+// ability to name pet
+
+// ablity to move pet on screen
+
+// ability to morph your pet at certain ages
+
+
+// Increase your pet's age every [how ever long you want].
+
+
+// display character
+
+
+// game start
+
+
+
+
+
+
+
+/// Event listeners
+
+
+
+
+
+
